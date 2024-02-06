a1e3e314f7236c50b9c65078c7e2153e5b07a080
Layeeq786 <153986931+Layeeq786@users.noreply.github.com>
2024-02-07 00:54:39 +0530

    quiz

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "You're 4th place right now in a race. What place will you be in when you pass the person in 3rd place?",
+    answer: "3rd",
+    options: [
+      "1st",
+      "2nd",
+      "3rd",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many months have 28 days?",
+    answer: "All of them",
+    options: [
+      "2",
+      "1",
+      "All of them",
+      "I not from this earth"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many 0.5cm slices can you cut from a bread that is 25cm long?",
+    answer: "50",
+    options: [
+      "50",
+      "25",
+      "39",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The answer is really big.",
+    answer: "THE ANSWER.",
+    options: [
+      "THE ANSWER.",
+      "Really big.",
+      "An elephant.",
+      "I don't have that much brain."
+    ]
+  },
+    {
+    numb: 5,
+    question: "Divide 30 by half and add ten.",
+    answer: "70",
+    options: [
+      "40.5",
+      "50",
+      "70",
+      "I don't have that much brain."
+    ]
+  },
+];
